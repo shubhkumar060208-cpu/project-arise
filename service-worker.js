02941c5bdbc9b4020d5b9257bb45ad2710646b76
shubhkumar060208-cpu <shubhkumar060208@gmail.com>
2026-01-15 14:40:59 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "arise-v1";
+const CACHE_NAME = "arise-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -8,14 +8,33 @@ const FILES_TO_CACHE = [
   "./style.css",
   "./script.js",
   "./manifest.json",
+
+  // icons
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+
+  // sounds
+  "./sounds/boot.wav",
+  "./sounds/click.wav"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
