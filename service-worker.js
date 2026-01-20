8a89d0d4f55fdeefe04e9e9896b085dd77734d35
shubhkumar060208-cpu <shubhkumar060208@gmail.com>
2026-01-20 14:46:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "arise-v1.52";
+const CACHE_NAME = "arise-v1.6";
 
 const FILES_TO_CACHE = [
   "./",
@@ -44,6 +44,7 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
 
 
 
